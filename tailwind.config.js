115d2d9f6dca4e74a417247f4b6566c7b40e678c
Tushar <nejetushar07@gmail.com>
2025-01-16 19:23:41 +0530

    Added New Buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-bg' : '#138ff4',
+      }
+    },
   },
   plugins: [],
 }
