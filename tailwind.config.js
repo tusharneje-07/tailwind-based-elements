8324cca745bff67db3fc0b762067b2a0e2e16b36
Tushar <nejetushar07@gmail.com>
2025-02-26 14:31:39 +0530

    Upgraded Chat bot Interface

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ module.exports = {
     extend: {
       colors: {
         'primary-bg' : '#138ff4',
-      }
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            color: '#111827', // Dark gray
+            'h1, h2, h3, h4, strong, bold': {
+              color: '#000000', // Black for headings
+            },
+            'a': {
+              color: '#1d4ed8', // Blue links
+              textDecoration: 'none',
+            },
+            h1: { margin:0 }, 
+            p: { margin:0 },
+            ul: { marginBottom: '0.0rem' },
+            li: { marginBottom: '0rem' },
+          },
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"),],
 }
